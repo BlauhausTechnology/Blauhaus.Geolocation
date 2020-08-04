899f0d4828aa24b58628d50d76dfdc36ddb46973
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-04 12:23:46 +0200

    no message

---
+++ src/Blauhaus.Geolocation/_Ioc/IocServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Blauhaus.Geolocation.Abstractions;
+using Blauhaus.Geolocation.Proxy;
+using Blauhaus.Ioc.Abstractions;
+
+namespace Blauhaus.Geolocation._Ioc
+{
+    public static class IocServiceExtensions
+    {
+        public static IIocService AddGeolocation(this IIocService iocService)
+        {
+            iocService.RegisterImplementation<IGeolocationService, GeolocationService>(IocLifetime.Singleton);
+            iocService.RegisterImplementation<IGeolocationProxy, XamarinEssentialsGeolocationProxy>();
+            return iocService;
+        }
+    }
+}
---
+++ src/Blauhaus.Geolocation/_Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Blauhaus.Geolocation.Abstractions;
+using Blauhaus.Geolocation.Proxy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.Geolocation._Ioc
+{
+    public static class ServiceCollectionExtensions 
+    {
+        public static IServiceCollection AddGeolocation(this IServiceCollection services)
+        {
+            services.AddSingleton<IGeolocationService, GeolocationService>();
+            services.AddTransient<IGeolocationProxy, XamarinEssentialsGeolocationProxy>();
+            return services;
+        }
+    }
+}
