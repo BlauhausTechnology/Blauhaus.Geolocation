142761857c2a0d2967e8ad0f0eed33e0a6cfdefb
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-06-24 20:17:06 +0200

    no message

--- src/Blauhaus.Geolocation.TestHelpers/GpsLocationMockBuilder.cs
+++ src/Blauhaus.Geolocation.TestHelpers/GpsLocationMockBuilder.cs
@@ -1,9 +1,22 @@
 ﻿using Blauhaus.Geolocation.Abstractions.ValueObjects;
 using Blauhaus.TestHelpers.MockBuilders;
+using Moq;
 
 namespace Blauhaus.Geolocation.TestHelpers
 {
     public class GpsLocationMockBuilder : BaseMockBuilder<GpsLocationMockBuilder, IGpsLocation>
     {
+        public GpsLocationMockBuilder Where_GetMetresFrom_returns(double value)
+        {
+            Mock.Setup(x => x.GetMetresFrom(It.IsAny<IGpsLocation>()))
+                .Returns(value);
+            return this;
+        }
+        public GpsLocationMockBuilder Where_GetMetresFrom_returns(double value, IGpsLocation other)
+        {
+            Mock.Setup(x => x.GetMetresFrom(other))
+                .Returns(value);
+            return this;
+        }
     }
 }
